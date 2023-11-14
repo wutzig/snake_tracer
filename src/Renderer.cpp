6a4aa3dfa92a24d4652fed1a40e7925d0bce18ce
Michael Wutzig <michael.wutzig@gmail.com>
2023-11-14 15:48:14 +0100

    set warnings in cmake

--- src/Renderer.cpp
+++ src/Renderer.cpp
@@ -25,14 +25,14 @@ Renderer::Renderer(){
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, element_buffer_id);
     const unsigned short elements[6] = {0, 1, 2, 0, 2, 3};
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(elements), &elements[0], GL_STATIC_DRAW);
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, NULL);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, GL_NONE);
 
     glCreateBuffers(1, &uvBufferID);
     glEnableVertexAttribArray(1);
     const float texCoords[] = {0.0f, 1.0f, 0.0f, 0.0f, 1.0f, 0.0f, 1.0f, 1.0f};
     glBindBuffer(GL_ARRAY_BUFFER, uvBufferID);
     glBufferData(GL_ARRAY_BUFFER, sizeof(texCoords), texCoords, GL_STATIC_DRAW);
-    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 0, NULL);
+    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 0, GL_NONE);
 
     glCreateTextures(GL_TEXTURE_2D, 1, &textureID);
     glActiveTexture(GL_TEXTURE0);
@@ -41,7 +41,7 @@ Renderer::Renderer(){
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA32F, WIDTH, HEIGHT, 0, GL_RGBA, GL_FLOAT, NULL);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA32F, WIDTH, HEIGHT, 0, GL_RGBA, GL_FLOAT, GL_NONE);
     glBindImageTexture(0, textureID, 0, GL_FALSE, 0, GL_WRITE_ONLY, GL_RGBA32F);
 
     int work_grp_cnt[3];
@@ -79,7 +79,7 @@ Renderer::Renderer(){
     glGetIntegerv(GL_MAX_COMBINED_SHADER_STORAGE_BLOCKS, &output);
     std::cout << "max total blocks: " << output << "\n";
 
-    glBindVertexArray(NULL);
+    glBindVertexArray(GL_NONE);
 }
 void Renderer::draw_scence(uint32_t shader_program_id){
     glClearColor(0.2f, 0.2f, 0.7f, 1.0f);
@@ -89,11 +89,11 @@ void Renderer::draw_scence(uint32_t shader_program_id){
     glBindVertexArray(vao_id);
     glBindTexture(GL_TEXTURE_2D, textureID);
     glEnableVertexAttribArray(0);
-    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_SHORT, NULL);
+    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_SHORT, GL_NONE);
     glDisableVertexAttribArray(0);
-    glBindTexture(GL_TEXTURE_2D, NULL);
-    glBindVertexArray(NULL);
-    glUseProgram(NULL);
+    glBindTexture(GL_TEXTURE_2D, GL_NONE);
+    glBindVertexArray(GL_NONE);
+    glUseProgram(GL_NONE);
 }
 
 void Renderer::use_compute_shader(uint32_t compute_program_id){
@@ -182,7 +182,7 @@ void Renderer::create_objects(uint32_t compute_program_id){
     glUniform3fv(locations.camera_up, 1, glm::value_ptr(camera_up));
     glUniform3fv(locations.camera_right, 1, glm::value_ptr(camera_right));
     glUniform3fv(locations.camera_position, 1, glm::value_ptr(camera_position));
-    glUseProgram(NULL);
+    glUseProgram(GL_NONE);
 }
 void Renderer::add_sphere(glm::vec3 position, glm::vec3 color, float radius){
     sphere_positions.push_back(position);
